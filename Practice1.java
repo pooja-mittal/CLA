d79d338822bded74b960a6b035afcb69e7714540
pooja-mittal <pooja.mittal30@gmail.com>
2018-02-26 12:50:22 +0530

    Add files via upload

---
+++ Practice1.java
@@ -0,0 +1,8 @@
+class Practice1
+{
+	public static void main(String[] args)
+	{
+		//System.out.println(args[0]);
+		System.out.println("args[6]");
+	}
+}
---
+++ Test3.java
@@ -0,0 +1,9 @@
+class Test3
+{
+	public static void main(String[] args)
+	{
+		for(int i=1; i<args.length; i++) {
+			System.out.print(args[i] + " " );
+		}
+	}
+}
